8497116d67fe73a4c387ec7a4847f5759bbe927c
Risolat <utkurova.risolat@mail.ru>
2020-07-21 22:54:50 +0500

    Updates

---
+++ precache-manifest.b081dc086c0945c897db0228ee7fd776.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f57240fe3ddf2c1448190c6991f0df52",
+    "url": "/auth-react/index.html"
+  },
+  {
+    "revision": "39873a7006dd385be498",
+    "url": "/auth-react/static/css/main.6b0d1ee6.chunk.css"
+  },
+  {
+    "revision": "f818a1a420fd07dc9a77",
+    "url": "/auth-react/static/js/2.4a0c96f5.chunk.js"
+  },
+  {
+    "revision": "971b4e562d3abe5fe0995509ffca7edb",
+    "url": "/auth-react/static/js/2.4a0c96f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39873a7006dd385be498",
+    "url": "/auth-react/static/js/main.8eca05ea.chunk.js"
+  },
+  {
+    "revision": "b27e5068f264dc93960a",
+    "url": "/auth-react/static/js/runtime-main.102457e8.js"
+  }
+]);
